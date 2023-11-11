9ffe2ad16017df40cec2a974644644f9cc2070f4
jair silva <harryhaller30@gmail.com>
2023-11-11 17:32:52 -0500

    first commi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevicesComponent } from './devices/devices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'devices', component: DevicesComponent },
+  { path: '', redirectTo: '/devices', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductCardsComponent } from './product-cards/product-cards.component';
+import { DevicesComponent } from './devices/devices.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardsComponent,
+    DevicesComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
